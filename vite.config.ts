c033b868444e5b257734b42939c3658dc342bcbc
Janier <janierss17@gmail.com>
2024-12-01 23:48:33 -0500

    federation config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), federation({
+    name: 'my-notes',
+    remotes: {},
+    shared: ['react', 'react-dom', 'react-router-dom', 'react-redux', '@reduxjs/toolkit']
+  })],
   server: {
     open: true,
     port: 3000
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
   }
 })
